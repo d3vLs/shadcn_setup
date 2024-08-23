6e0e6a50663f695e90e68455fec1dcb2b9efdf4a
D3vlS <dhruv.sahani.1807@gmail.com>
2024-08-23 23:44:21 +0530

    new file:   app/globals.css 	new file:   components.json 	new file:   jsconfig.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/ui/button.jsx 	new file:   src/components/ui/calendar.jsx 	new file:   src/components/ui/calender.jsx 	new file:   src/globals.css 	new file:   src/lib/utils.js 	new file:   tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import "./App.css";
+import "./globals.css";
+import { Calendar } from "./components/ui/calendar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className="bg-gray-700 ">
+        {/* <p className="">Working</p> */}
+        <Calendar />
+      </div>
     </div>
   );
 }
